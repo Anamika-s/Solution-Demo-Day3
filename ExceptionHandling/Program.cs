01233df9cca88091b9fc9dc9695e08fa840e4fd1
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-07-30 11:06:29 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, res = 0;
+            try
+            {
+                Console.WriteLine("enter num ");
+                num1 = byte.Parse(Console.ReadLine());
+                Console.WriteLine("enter den");
+                num2 = byte.Parse(Console.ReadLine());
+                res = num1 / num2;
+
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Only numbers are allowed");
+            }
+             catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("cannot divide num by 0");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("NUm is outside the range");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Res is" + res);
+                Console.WriteLine("END OF PROG");
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/Program2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program2
+    {
+        static void Main()
+        {
+            int rn=0;
+            string name = string.Empty;
+            string batch = string.Empty;
+            int score=0;
+            try
+            {
+                Console.WriteLine("enter rn");
+                rn = byte.Parse(Console.ReadLine());
+                if (rn < 0 || rn > 20)
+                    throw new CustomException("Rn can come in 1 to 20 range only");
+                Console.WriteLine("enter name");
+                name = Console.ReadLine();
+                if (name.Length < 10)
+                    throw new CustomException("MIn 10 chacracters needed");
+                Console.WriteLine("enter batch");
+                batch = Console.ReadLine();
+                Console.WriteLine("enter score");
+                score = byte.Parse(Console.ReadLine());
+                if (score < 0 || score > 100)
+                    throw new CustomException("Score can come in 0 to 100 range only");
+
+            }
+            catch(CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("rn is " + rn);
+                Console.WriteLine("name is " + name);
+                Console.WriteLine("batch is " + batch);
+                Console.WriteLine("score is " + score);
+            }
+        }
+    }
+}
